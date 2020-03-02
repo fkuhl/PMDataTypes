6b2327dc99cd8a7a19a11ac61bda949f2e56c644
Frederick Kuhl <fkuhl@tyndalesoft.com>
2020-03-02 09:14:59 -0500

    try consolidating id field on HouseholdDocument

--- Sources/PMDataTypes/HouseholdDocument.swift
+++ Sources/PMDataTypes/HouseholdDocument.swift
@@ -12,15 +12,6 @@ import Foundation
  */
 public struct HouseholdDocument: Codable {
     public var id: Id
-    public var value: HouseholdDocumentValue
-    
-    public init(id: Id, value: HouseholdDocumentValue) {
-        self.id = id
-        self.value = value
-    }
-}
-
-public struct HouseholdDocumentValue: Codable {
     public var head: Member = Member() //data cleaned up enough so this isn't ever nil
     public var spouse: Member? = nil
     public var others: [Member] = []
@@ -32,3 +23,16 @@ public struct HouseholdDocumentValue: Codable {
         return try! jsonEncoder.encode(self)
     }
 }
+
+//public struct HouseholdDocumentValue: Codable {
+//    public var head: Member = Member() //data cleaned up enough so this isn't ever nil
+//    public var spouse: Member? = nil
+//    public var others: [Member] = []
+//    public var address: Address? = nil //nil if address unknown
+//
+//    public init() { }
+//
+//    public func asJSONData() -> Data  {
+//        return try! jsonEncoder.encode(self)
+//    }
+//}
