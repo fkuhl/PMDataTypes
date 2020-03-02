5d4071209106d96aa19385d7dc4d24fb98557243
Frederick Kuhl <fkuhl@tyndalesoft.com>
2020-03-02 09:04:01 -0500

    StructureName added

--- Sources/PMDataTypes/CollectionName.swift
+++ Sources/PMDataTypes/CollectionName.swift
@@ -12,7 +12,5 @@ import Foundation
  */
 
 public enum CollectionName: String, CaseIterable {
-//    case members = "Members"
     case households = "Households"
-//    case addresses = "Addresses"
 }
---
+++ Sources/PMDataTypes/StructureName.swift
@@ -0,0 +1,16 @@
+//
+//  StructureName.swift
+//  
+//
+//  Created by Frederick Kuhl on 3/2/20.
+//
+
+import Foundation
+
+/**
+ URL element values corresponding to the two data structures that can be asked for.
+ */
+public enum StructureName: String {
+    case members = "Members"
+    case households = "Households"
+}
