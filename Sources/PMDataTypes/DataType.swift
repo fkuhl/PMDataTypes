eeb2f103b1339f0a6692fb26f5d1ac7867cb3a18
Frederick Kuhl <fkuhl@tyndalesoft.com>
2020-05-20 13:49:48 -0400

    simplify date formatter

--- Sources/PMDataTypes/DataType.swift
+++ Sources/PMDataTypes/DataType.swift
@@ -16,9 +16,10 @@ import Foundation
 public let dateFormatter: DateFormatter = {
     let formatter = DateFormatter()
     formatter.dateFormat = "yyyy-MM-dd"
-    formatter.calendar = Calendar(identifier: .iso8601)
-    formatter.timeZone = TimeZone(secondsFromGMT: 0)
-    formatter.locale = Locale(identifier: "en_US_POSIX")
+    //Some combo of the below was leading to off-by-one errors in the day of month.
+//    formatter.calendar = Calendar(identifier: .iso8601)
+//    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+//    formatter.locale = Locale(identifier: "en_US_POSIX")
     return formatter
 }()
 
